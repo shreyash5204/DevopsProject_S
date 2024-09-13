3a11438faab0c8722cfd8453881000403c6b1f78
tathya09 <106487768+tathya09@users.noreply.github.com>
2024-09-13 17:12:56 +0530

    Added by Tathya

--- python.py
+++ python.py
@@ -7,3 +7,12 @@ sum = num1 + num2
 
 # printing values
 print("Sum of", num1, "and", num2 , "is", sum)
+
+# Initialize a list Done By Tathya
+my_list = [1, 2, 3, 4, 5]
+
+# Interchange first and last elements  Done By Tathya
+my_list[0], my_list[-1] = my_list[-1], my_list[0]
+
+# Print the modified list  Done By Tathya
+print("List after swapping first and last elements:", my_list)
