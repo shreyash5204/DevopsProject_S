e2acfe009c688d2ae683f19a7348008758906b16
Ashsync <ak14ashkr@gmail.com>
2024-09-13 18:27:27 +0530

    commented out one line

--- python.py
+++ python.py
@@ -16,3 +16,4 @@ my_list[0], my_list[-1] = my_list[-1], my_list[0]
 
 # Print the modified list  Done By Tathya
 print("List after swapping first and last elements:", my_list)
+# this is me ashish  
