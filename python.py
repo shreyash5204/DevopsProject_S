ead3a8d4c62a92aec74797abb30e90a0b38e685b
Ayush Dubey <ayushdubey0806@gmail.com>
2024-09-13 19:19:44 +0530

    Simple Guessing game

--- python.py
+++ python.py
@@ -25,3 +25,25 @@ if n1 % 2==0:
     print("This is Even number")
 else:
     print("This is Odd number")
+
+
+
+# Python program for a simple guessing game by Ayush Dubey
+import random
+
+def guessing_game():
+    number_to_guess = random.randint(1, 10)
+    attempts = 0
+    guess = None
+
+    while guess != number_to_guess:
+        guess = int(input("Guess a number between 1 and 10: "))
+        attempts += 1
+        if guess < number_to_guess:
+            print("Too low!")
+        elif guess > number_to_guess:
+            print("Too high!")
+        else:
+            print(f"Congratulations! You guessed the number in {attempts} attempts.")
+
+guessing_game()
